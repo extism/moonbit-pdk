a026354b341a5eacacddcf8b167cb71659c8c4da
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-06-02 15:44:54 -0400

    count-vowels works in browser
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ex
 
-# moon build --target wasm
-#
+# wasm-gc is useful for debugging in the browser:
+moon build --target wasm-gc
+
 # Due to this current MoonBit issue: https://github.com/moonbitlang/core/issues/480
 # it is necessary to replace the "spectest.print_char" external import with an
 # internal substitution, so this Go program performs that task:
