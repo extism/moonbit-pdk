e77e8884aab1a2acbdd88f6b5749e2dacdfe1d38
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-07-09 09:31:31 -0400

    Remove moon compiler workaround
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 # wasm-gc can be useful for debugging in the browser:
 # moon build --target wasm-gc --output-wat
 # moon build --target wasm-gc
+# moon build --target wasm --output-wat
 
-# Due to this current MoonBit issue: https://github.com/moonbitlang/core/issues/520
-# it is necessary to replace the `export "_start"` with `export "_initialize"`,
-# so this Go program performs that task:
-moon build --target wasm --output-wat
-go run cmd/change-start/main.go --watflags="--debug-names"
+moon build --target wasm
