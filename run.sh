ea80d82ce78a241d6d6abe03c70743d4b0218d83
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-08-21 23:09:41 -0400

    Update run.sh
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+./scripts/add.sh '{"a": 20, "b": 21}' && echo && echo
 ./scripts/greet.sh Benjamin && echo && echo
 ./scripts/count-vowels.sh 'Once upon a dream' && echo && echo
 ./scripts/http-get.sh && echo && echo
