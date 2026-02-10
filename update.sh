22bbe412707fb974e0769aced163cf4b1e6ded15
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-02-10 18:36:28 -0500

    Update to moonc v0.8.1+bd827dc85
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
-moon update && moon install && rm -rf target
+moon update && rm -rf target
 moon fmt && moon info
 # As of moonc v0.6.31+b5b06ff93, `--target native` no longer works.
 # moon test --target all
-moon test --target wasm-gc
-moon test --target wasm
-moon test --target js
+moon test -j 12 --target wasm-gc
+moon test -j 12 --target wasm
+moon test -j 12 --target js
